03a1504d2212cd5d71b5526fac9b2ecdcf4d5f7c
Mufaddal Makati <mufaddal@rawbytes.com>
2025-07-15 16:30:36 -0400

    Get a list of your recently opened files

--- src/main/java/com/rawbytes/recentfiles/RecentFilesToolWindowFactory.java
+++ src/main/java/com/rawbytes/recentfiles/RecentFilesToolWindowFactory.java
@@ -9,13 +9,20 @@ import com.intellij.ui.components.JBList;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.ui.JBColor;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.ui.components.JBScrollPane;
+import com.intellij.openapi.vfs.VfsUtilCore;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
+
 /**
  * Factory class for creating the Recent Files tool window.
  */
@@ -32,24 +39,14 @@ public class RecentFilesToolWindowFactory implements ToolWindowFactory {
         // Get the list of recently opened files
         RecentFilesService recentFilesService = project.getService(RecentFilesService.class);
 
+
         // Create a list model to hold the files
         DefaultListModel<VirtualFile> listModel = new DefaultListModel<>();
         listModel.addAll(recentFilesService.getRecentFiles());
 
         // Create a JBList to display the files
         JBList<VirtualFile> fileList = new JBList<>(listModel);
-        fileList.setCellRenderer(new DefaultListCellRenderer() {
-            @Override
-            public java.awt.Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-                super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-                if (value instanceof VirtualFile file) {
-                    setText(file.getPath());
-                    setIcon(file.getFileType().getIcon());
-                    setToolTipText(file.getPath());
-                }
-                return this;
-            }
-        });
+        fileList.setCellRenderer(new RecentFileCellRenderer(project));
 
         // Add a mouse listener to open the file on double-click
         fileList.addMouseListener(new MouseAdapter() {
@@ -80,8 +77,56 @@ public class RecentFilesToolWindowFactory implements ToolWindowFactory {
                     listModel.clear();
                     listModel.addAll(files);
                 });
-        };
-    });
-}}
+            }
 
+            ;
+        });
+    }
 
+    /**
+     * A custom cell renderer that displays the file name on the first line
+     * and its parent directory path on a smaller, subdued second line.
+     */
+    private static class RecentFileCellRenderer extends JPanel implements ListCellRenderer<VirtualFile> {
+        private final JLabel fileNameLabel = new JLabel();
+        private final JLabel filePathLabel = new JLabel();
+        private final Project project;
+
+        RecentFileCellRenderer(Project project) {
+            super(new BorderLayout(0, 2));
+            this.project = project;
+            setBorder(JBUI.Borders.empty(5));
+            filePathLabel.setForeground(JBColor.GRAY);
+            filePathLabel.setFont(JBUI.Fonts.smallFont());
+            add(fileNameLabel, BorderLayout.NORTH);
+            add(filePathLabel, BorderLayout.SOUTH);
+        }
+
+        @Override
+        public Component getListCellRendererComponent(JList<? extends VirtualFile> list,
+                                                      VirtualFile file,
+                                                      int index,
+                                                      boolean isSelected,
+                                                      boolean cellHasFocus) {
+            fileNameLabel.setText(file.getPresentableName());
+            fileNameLabel.setIcon(file.getFileType().getIcon());
+
+            VirtualFile projectBase = project.getBaseDir();
+            String relPath = VfsUtilCore.getRelativePath(file, projectBase, '/');
+            filePathLabel.setText(relPath != null ? relPath : file.getPresentableUrl());
+            setToolTipText(file.getUrl());
+
+            if (isSelected) {
+                setBackground(UIUtil.getListSelectionBackground(cellHasFocus));
+                fileNameLabel.setForeground(UIUtil.getListSelectionForeground(cellHasFocus));
+                filePathLabel.setForeground(UIUtil.getListSelectionForeground(cellHasFocus));
+            } else {
+                setBackground(UIUtil.getListBackground());
+                fileNameLabel.setForeground(UIUtil.getListForeground());
+                filePathLabel.setForeground(JBColor.GRAY);
+            }
+
+            return this;
+        }
+    }
+}
